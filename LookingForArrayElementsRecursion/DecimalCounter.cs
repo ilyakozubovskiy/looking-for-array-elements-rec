c5f8827fe00426e6faa394480ae6fe3a9b85fab4
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-01-11 15:47:15 +0300

    Looking for Array Elements (Recursion)

--- LookingForArrayElementsRecursion/DecimalCounter.cs
+++ LookingForArrayElementsRecursion/DecimalCounter.cs
@@ -14,8 +14,42 @@ namespace LookingForArrayElementsRecursion
         /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
         public static int GetDecimalsCount(decimal[] arrayToSearch, decimal[][] ranges)
         {
-            // TODO #5. Implement the method using recursion.
-            throw new NotImplementedException();
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (ranges is null)
+            {
+                throw new ArgumentNullException(nameof(ranges));
+            }
+
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                if (ranges[i] == null)
+                {
+                    throw new ArgumentNullException(nameof(ranges));
+                }
+
+                if (ranges[i].Length != 0 && ranges[i].Length != 2)
+                {
+                    throw new ArgumentException(" The length of one of the ranges is less or greater than 2 or not equal to zero  ");
+                }
+            }
+
+            if (arrayToSearch.Length == 0 || ranges.Length == 0)
+            {
+                return 0;
+            }
+
+            decimal[] suitable = Array.Empty<decimal>();
+
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                FindSuitableElements(ref suitable, ranges, arrayToSearch, 0);
+            }
+
+            return suitable.Length + GetDecimalsCount(arrayToSearch[1..], ranges);
         }
 
         /// <summary>
@@ -28,8 +62,88 @@ namespace LookingForArrayElementsRecursion
         /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
         public static int GetDecimalsCount(decimal[] arrayToSearch, decimal[][] ranges, int startIndex, int count)
         {
-            // TODO #6. Implement the method using recursion.
-            throw new NotImplementedException();
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (ranges is null)
+            {
+                throw new ArgumentNullException(nameof(ranges));
+            }
+
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                if (ranges[i] == null)
+                {
+                    throw new ArgumentNullException(nameof(ranges));
+                }
+
+                if (ranges[i].Length != 0 && ranges[i].Length != 2)
+                {
+                    throw new ArgumentException(" The length of one of the ranges is less or greater than 2 or not equal to zero");
+                }
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            decimal[] suitable = Array.Empty<decimal>();
+
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                FindSuitableElements(ref suitable, ranges, arrayToSearch, startIndex);
+            }
+
+            return suitable.Length + GetDecimalsCount(arrayToSearch[1..], ranges, startIndex, count - 1);
+        }
+
+        private static bool HasDublicates(decimal[] array, decimal value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void FindSuitableElements(ref decimal[] suitable, decimal[][] ranges, decimal[] arrayToSearch, int index)
+        {
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                bool rangeCriteria = ranges[j].Length != 0 && arrayToSearch[index] >= ranges[j][0] && arrayToSearch[index] <= ranges[j][1];
+                if (rangeCriteria && !HasDublicates(suitable, arrayToSearch[index]))
+                {
+                    Array.Resize(ref suitable, suitable.Length + 1);
+                    suitable[^1] = arrayToSearch[index];
+                }
+            }
         }
     }
 }
--- LookingForArrayElementsRecursion/FloatCounter.cs
+++ LookingForArrayElementsRecursion/FloatCounter.cs
@@ -13,8 +13,51 @@ namespace LookingForArrayElementsRecursion
         /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
         public static int GetFloatsCount(float[] arrayToSearch, float[] rangeStart, float[] rangeEnd)
         {
-            // TODO #3. Implement the method using recursion.
-            throw new NotImplementedException();
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (rangeStart is null)
+            {
+                throw new ArgumentNullException(nameof(rangeStart));
+            }
+
+            if (rangeEnd is null)
+            {
+                throw new ArgumentNullException(nameof(rangeEnd));
+            }
+
+            if (rangeStart.Length == 0 || rangeEnd.Length == 0)
+            {
+                return 0;
+            }
+
+            if (rangeStart.Length != rangeEnd.Length)
+            {
+                throw new ArgumentException("Arrays of range starts and range ends contain different number of elements.");
+            }
+
+            if (rangeStart[0] > rangeEnd[0] || rangeStart[^1] > rangeEnd[^1])
+            {
+                throw new ArgumentException("The range start value is greater than the range end value.");
+            }
+
+            if (arrayToSearch.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < rangeStart.Length; i++)
+            {
+                if (arrayToSearch[0] >= rangeStart[i] && arrayToSearch[0] <= rangeEnd[i])
+                {
+                    sum++;
+                }
+            }
+
+            return sum + GetFloatsCount(arrayToSearch[1..], rangeStart, rangeEnd);
         }
 
         /// <summary>
@@ -28,8 +71,71 @@ namespace LookingForArrayElementsRecursion
         /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
         public static int GetFloatsCount(float[] arrayToSearch, float[] rangeStart, float[] rangeEnd, int startIndex, int count)
         {
-            // TODO #4. Implement the method using recursion.
-            throw new NotImplementedException();
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (rangeStart is null)
+            {
+                throw new ArgumentNullException(nameof(rangeStart));
+            }
+
+            if (rangeEnd is null)
+            {
+                throw new ArgumentNullException(nameof(rangeEnd));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (rangeStart.Length == 0 || rangeEnd.Length == 0)
+            {
+                return 0;
+            }
+
+            if (rangeStart.Length != rangeEnd.Length)
+            {
+                throw new ArgumentException("Arrays of range starts and range ends contain different number of elements.");
+            }
+
+            if (rangeStart[0] > rangeEnd[0] || rangeStart[^1] > rangeEnd[^1])
+            {
+                throw new ArgumentException("The range start value is greater than the range end value.");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < rangeStart.Length; i++)
+            {
+                if (arrayToSearch[startIndex] >= rangeStart[i] && arrayToSearch[startIndex] <= rangeEnd[i])
+                {
+                    sum++;
+                }
+            }
+
+            return sum + GetFloatsCount(arrayToSearch[1..], rangeStart, rangeEnd, startIndex, count - 1);
         }
     }
 }
